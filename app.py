ad7b13257edcfdebd1f1d8c1b4cd671861872c3f
Ngo Tran Yen Vi <233404051128@gmail.com>
2026-09-06 13:05:32 +0700

    feat: add API counter and info

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import os
+import datetime
+import platform
 from flask import Flask
-import datetime, platform
 
 app = Flask(__name__)
+
+visit_count = 0  # Biến đếm lưu trong RAM của container
+
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
+    return {
+        "sinh_vien": ten_sinh_vien,
+        "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
 
 @app.route("/")
 def home():
@@ -14,7 +35,7 @@ def home():
     </style></head><body>
     <h1>Ung dung Flask tren PaaS</h1>
     <div class="box">
-    <p><b>Sinh vien:</b> NGO TRAN YEN VI (v2)– 233404051128</p>
+    <p><b>Sinh vien:</b> NGO TRAN YEN VI 233404051128</p>
     <p><b>Mon hoc:</b> Dien toan Dam may</p>
     <p><b>Mo hinh:</b> PaaS – Platform as a Service</p>
     <p><b>Python:</b> {platform.python_version()}</p>
@@ -23,10 +44,3 @@ def home():
     <p>Developer chi viet code – PaaS lo build, deploy, HTTPS, scaling!</p>
     </body></html>
     """
-
-@app.route("/health")
-def health():
-    return {"status": "ok"}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
